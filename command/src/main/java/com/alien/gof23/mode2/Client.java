ca18b6f9075adaf9e976aa0a81ed21fd3f6895f3
alien <15080165456@163.com>
2019-09-15 23:25:43 +0800

    命令模式模板

--- command/src/main/java/com/alien/gof23/mode2/Client.java
+++ command/src/main/java/com/alien/gof23/mode2/Client.java
@@ -2,8 +2,10 @@ package com.alien.gof23.mode2;
 
 /**
  * program: gof23
- * description: 请求者
- *
+ * description: 请求者 <br/>
+ * 请求者 创建 命令 <br/>
+ * 发动者 执行 命令 <br/>
+ * 接受者 实施 命令 <br/>
  * @author: alien
  * @since: 2019/09/15 23:20
  */
