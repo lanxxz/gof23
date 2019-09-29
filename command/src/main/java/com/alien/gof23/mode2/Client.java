4950eafa99b1fa3e396ebcf9826537b3f669b156
alien <15080165456@163.com>
2019-09-29 23:13:42 +0800

    听说更新macOS后Git不能用了提交一个试试看

--- command/src/main/java/com/alien/gof23/mode2/Client.java
+++ command/src/main/java/com/alien/gof23/mode2/Client.java
@@ -12,6 +12,7 @@ package com.alien.gof23.mode2;
 public class Client {
 
     public static void main(String[] args) {
+
         Invoker invoker = new Invoker();
         Receiver receiver = new ConcreteReciver1();
         Command command = new ConcreteCommand1(receiver);
